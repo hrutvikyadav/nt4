05cbb484176cab5a3417c91e1389098793dd55d6
hrutvikyadav <hrutvikyadav@gmail.com>
2025-08-06 17:26:45 +0530

    chore: indentscope

--- lua/riaari/mini.lua
+++ lua/riaari/mini.lua
@@ -4,7 +4,7 @@ local indentscope_opts = {
   -- Draw options
   draw = {
     -- Delay (in ms) between event and start of drawing scope indicator
-    delay = 100,
+    delay = 400,
 
     -- Animation rule for scope's first drawing. A function which, given
     -- next and total step numbers, returns wait time (in ms). See
@@ -52,7 +52,7 @@ local indentscope_opts = {
 
   -- Which character to use for drawing scope indicator
     -- '┊', `┋`,  `▏`, `│`, '╎', `▍`
-  symbol = '▍',
+  symbol = '│',
 }
 
 function M.config()
