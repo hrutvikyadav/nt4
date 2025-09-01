61b8d6707a1e22da61e450e5f3d23d49a5ff071e
hrutvikyadav <hrutvikyadav@gmail.com>
2025-09-01 11:42:22 +0530

    chore: qol

--- lua/riaari/autocommands.lua
+++ lua/riaari/autocommands.lua
@@ -20,5 +20,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
     vim.opt_local.shiftwidth = 2
+    vim.opt.listchars = { tab = '<->', lead = '»', trail = '·', nbsp = '␣' ,multispace = '•', leadmultispace = '»·'}
   end,
 })
